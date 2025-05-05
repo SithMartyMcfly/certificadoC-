887e19eb753324fb1595bd8532ee620baf392fbb
Sith_Mcfly <antogutilebron@gmail.com>
2025-05-05 21:22:30 +0200

    Recorridos Foreach
    uso Foreach con diferentes acciones y tipos

---
+++ RecorridosForeach/Program.cs
@@ -0,0 +1,28 @@
+﻿string[] names = { "Antonio", "Cristina", "Roberto" };
+Console.WriteLine("-----------------");
+foreach (string name in names) { 
+    Console.WriteLine(name);
+}
+
+int[] inventory = {200, 450, 700, 175, 250 };
+Console.WriteLine("-----------------");
+foreach (int item in inventory) {
+    Console.WriteLine(item);
+}
+
+int sum = 0;
+Console.WriteLine("-----------------");
+foreach (int item in inventory) {
+    sum += item;
+}
+Console.WriteLine("tenemos un total de " + sum);
+
+int bin = 0;
+sum = 0;
+Console.WriteLine("-----------------");
+    foreach (int item in inventory) {
+    sum += item;
+    bin++;
+    Console.WriteLine($"contenedor {bin} tiene {item} (Actualmente con un total de = {sum})");
+}
+Console.WriteLine("-----------------");
