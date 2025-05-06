65c970346d7eaae197ddc9354e9b6d746062dc82
Sith_Mcfly <antogutilebron@gmail.com>
2025-05-06 09:21:28 +0200

    identificadores de pedido
    Desafío de código en que debemos recoger algunos ids de un array

---
+++ Desafio de Codigo_identificadores de pedido/Program.cs
@@ -0,0 +1,17 @@
+﻿
+string[] idDelibery = {
+    "B123",
+    "C234",
+    "A345",
+    "C15",
+    "B177",
+    "G3003",
+    "C235",
+    "B179"
+};
+Console.WriteLine("Los pedidos que deben revisarse son:");
+foreach (string id in idDelibery) {
+    if (id.StartsWith("B")) {
+        Console.WriteLine(id);    
+    }
+}
