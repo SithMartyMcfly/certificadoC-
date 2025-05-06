48c900c671d9191a8545261740c279325692aed3
Sith_Mcfly <antogutilebron@gmail.com>
2025-05-06 21:26:42 +0200

    Proyecto Desafío
    Proyecto desafío que simula un combate entre dos jugadores

---
+++ 3.9 Proyecto desafio_batalla rol/Program.cs
@@ -0,0 +1,34 @@
+﻿int playerLife = 20;
+int monsterLife = 20;
+
+Random attack = new Random();
+
+do
+{
+    int playerAttack = 0;
+    playerAttack = attack.Next(1, 11);
+    monsterLife = monsterLife - playerAttack;
+    Console.WriteLine($"Monster was damage and lost **{playerAttack}** health,-- ({monsterLife}) -- health remaining");
+
+    if (monsterLife > 0)
+    {
+        int monsterAttack = 0;
+        monsterAttack = attack.Next(1, 11);
+        playerLife = playerLife - monsterAttack;
+        Console.WriteLine($"--Hero was damage and lost **{monsterAttack}** health,-- ({monsterLife}) -- health remaining");
+    }
+    else
+    {
+        break;
+    }
+
+} while (playerLife > 0 && monsterLife > 0);
+
+if (playerLife > 0)
+{
+    Console.WriteLine("Hero wins!");
+}
+else
+{
+    Console.WriteLine("Monster wins!");
+}
