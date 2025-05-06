0dd28c67d03d449b1a33339a8317c387cfeaae53
Sith_Mcfly <antogutilebron@gmail.com>
2025-05-06 14:00:49 +0200

    Proyecto desafío
    Proyecto para con un operador ternario usar un random para presentar cara o cruz

---
+++ 3.4 Proyecto desafio_caraOcruz/Program.cs
@@ -0,0 +1,10 @@
+﻿Random coin = new Random();
+
+int result = coin.Next(2);
+Console.WriteLine(result>0 ? "HEAD" : "TAILS");
+
+//otra solución
+/*string coinResult(int result) => result > 0 ? "HEAD" : "TAILS"; 
+Console.WriteLine(coinResult(result));*/
+
+
