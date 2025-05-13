0ae099f497a95b2d19dfe9310647da18130936f2
Sith_Mcfly <antogutilebron@gmail.com>
2025-05-13 14:37:11 +0200

    Proyecto desafio
    combinar valores de una matriz y operar diferentes tipos

---
+++ 4.3 Proyecto desafio_combinar valores de matriz/Program.cs
@@ -0,0 +1,20 @@
+﻿string[] values = { "12,3", "45", "ABC", "11", "DEF" };
+
+string cadena = "";
+decimal resultado = 0m;
+
+/*foreach(var value in values) usar var cuando las variables cambiarán de tipo*/ 
+for (int i = 0; i <values.Length;  i++) {
+    decimal numero;//variable auxiliar para el método TryParse
+    if (decimal.TryParse(values[i], out numero))//tengo que operar con la variable devuelta en TryParse
+    {
+        resultado += numero;
+    }
+    else
+    { //si no es número, lo concateno
+        cadena += values[i];
+    }
+}
+
+Console.WriteLine($"La suma de los números es: {resultado}");
+Console.WriteLine($"La concatenación de los textos es: {cadena}");
