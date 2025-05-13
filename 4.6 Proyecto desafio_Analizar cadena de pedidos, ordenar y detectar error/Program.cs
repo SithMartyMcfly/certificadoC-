c72a46ab149e5cb8fb5ec6099473f3cb646b0de3
Sith_Mcfly <antogutilebron@gmail.com>
2025-05-13 14:43:29 +0200

    Proyecto desafio
    Detectar en un Array de pedidos los Ids erróneos de los mismos

---
+++ 4.6 Proyecto desafio_Analizar cadena de pedidos, ordenar y detectar error/Program.cs
@@ -0,0 +1,22 @@
+﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+//troceamos el string
+string[] orderSplit = orderStream.Split(',');
+//ordenamos el array
+Array.Sort(orderSplit);
+
+//recorremos el array
+foreach (var orderId in orderSplit)
+{
+    //comprobamos si el id de la orden tiene 4 caracteres
+    //vamos imprimiendo según el resultado controlando con un if/else
+    if (orderId.Length == 4)
+    {
+        Console.WriteLine(orderId + "\t- OK");
+
+    }
+    else 
+    { 
+    Console.WriteLine(orderId + "\t- Error");
+    }
+}
