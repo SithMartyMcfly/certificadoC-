69fb7a65769d09061347b22a6877c7ea38871e50
Sith_Mcfly <antogutilebron@gmail.com>
2025-05-13 14:41:45 +0200

    Proyecto desafio
    Invertir las palabras cada palabra contenida en un array

---
+++ 4.5 Proyecto desafio_invertir cada palabra/Program.cs
@@ -0,0 +1,18 @@
+﻿string pangram = "The quick brown fox jumps over the lazy dog";
+//dividimos la cadena y la almacenamos en un array
+string[] words = pangram.Split(' ');
+
+
+foreach (string palabra in words) {
+    //troceamos la palabra
+    char[] nuevoArray = palabra.ToCharArray();
+    //invertimos el array (es la palabra suelta)
+    Array.Reverse(nuevoArray);
+    //concatenamos el array invertido en una cadena
+    string palabraReverse = string.Concat(nuevoArray);
+    //formateamos la cadena con un espacio entre palabras
+    Console.Write(palabraReverse + " ");
+}
+
+
+
